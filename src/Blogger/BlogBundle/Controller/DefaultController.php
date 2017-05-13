fe329fafaf13a80c1729b4ac7694fbfba72c0700
jack <x2002uwh@gmail.com>
2017-05-13 09:31:00 -0400

    init blog bundle

---
+++ src/Blogger/BlogBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('BloggerBlogBundle:Default:index.html.twig');
+    }
+}
